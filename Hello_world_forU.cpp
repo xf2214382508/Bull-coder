9882cc7ee9e453978eceb522f9eb45fa784b62dd
xf2214382508 <2214382508@qq.com>
2021-08-06 12:58:09 +0800

    Hello_world_forU.cpp

---
+++ Hello_world_forU.cpp
@@ -0,0 +1,19 @@
+//此例题出现了字符串处理相关知识，目前解决不了！ 
+
+#include<iostream>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+	string str1;
+	getline(cin,str1);
+	
+	
+//	printf("%s",str1);
+	cout<<str1<<endl;
+	
+	return 0;
+}
